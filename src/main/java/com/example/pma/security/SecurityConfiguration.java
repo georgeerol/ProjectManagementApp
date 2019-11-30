bf7d7b51425f5d86416c79d56819d521c450b86c
George Fouche <fouliex@gmail.com>
2019-11-30 13:24:51 -0800

    Add Spring Security

---
+++ src/main/java/com/example/pma/security/SecurityConfiguration.java
@@ -0,0 +1,69 @@
+package com.example.pma.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by George Fouche on 11/29/19.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
+                .withUser("george")
+                .password("pass")
+                .roles("USER")
+                .and()
+                .withUser("erol")
+                .password("pass2")
+                .roles("USER")
+                .and()
+                .withUser("steph")
+                .password("pass")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/projects/new").hasRole("USER")
+                .antMatchers("/employees/new").hasRole("USER")
+                .antMatchers(HttpMethod.POST,"/employees/save").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/").authenticated().and().formLogin();
+    }
+
+
+    @Override
+    public void configure(WebSecurity webSecurity) {
+        webSecurity
+                .ignoring()
+                .antMatchers(
+                        HttpMethod.POST,"/");
+    }
+
+}
